6bb4186784212b991d4205d75dc80fda4fb5e326
Simon Kinslow <simon.kinslow@UK-PX-00078.local>
2016-07-01 17:45:00 +0100

    Header added along with fade FX

--- public/js/col4.js
+++ public/js/col4.js
@@ -1,6 +1,24 @@
 var trace = function(str){ console.log(str); };
 
+var displayList;
+var tt;
+
 function page_init()
 {
 	trace("page_init();");
+
+	displayList_init();
+}
+
+function displayList_init()
+{
+	displayList = {};
+	displayList.mainHeader = document.querySelector(".page-content header h1");
+
+	tt = setTimeout(entrance_init, 0.5 * 1000);
+}
+
+function entrance_init()
+{
+	displayList.mainHeader.classList.remove("h1-default");
 }
