00d222763fe91499fb2673a8078038109dec4a1d
Simon Kinslow <simon.kinslow@UK-PX-00078.local>
2016-07-05 09:51:37 +0100

    Navigation interactions added
    Basic open and close added via CSS and JS

--- public/js/col4.js
+++ public/js/col4.js
@@ -2,6 +2,7 @@ var trace = function(str){ console.log(str); };
 
 var displayList;
 var tt;
+var nav;
 
 function page_init()
 {
@@ -13,7 +14,12 @@ function page_init()
 function displayList_init()
 {
 	displayList = {};
-	displayList.mainHeader = document.querySelector(".page-content header h1");
+	displayList.body		= document.querySelector("body");
+	displayList.pageWrapper = document.querySelector(".page-wrapper"); 
+	displayList.mainHeader 	= document.querySelector(".page-content header h1");
+	displayList.navIcon		= document.querySelector(".page-content .nav-icon");
+
+	nav_init();
 
 	tt = setTimeout(entrance_init, 0.5 * 1000);
 }
@@ -21,4 +27,44 @@ function displayList_init()
 function entrance_init()
 {
 	displayList.mainHeader.classList.remove("h1-default");
+}
+
+function nav_init()
+{
+	nav = {};
+	nav.open = false;
+
+	displayList.navIcon.addEventListener("click", nav_event, false);
+}
+
+function nav_event(event)
+{
+	var exitFrame;
+
+	if(nav.open)
+	{
+		nav.open = false;
+
+		displayList.body.classList.remove("nav-lock");
+		displayList.pageWrapper.classList.remove("page-wrapper-nav-open");
+
+		displayList.pageWrapper.removeEventListener("click", nav_event, false);
+	}
+
+	else
+	{
+		nav.open = true;
+
+		displayList.body.classList.add("nav-lock");
+		displayList.pageWrapper.classList.add("page-wrapper-nav-open");
+
+		// nav_help();
+		
+		exitFrame = setTimeout(nav_help, 20);
+	}
+}
+
+function nav_help()
+{
+	displayList.pageWrapper.addEventListener("click", nav_event, false);
 }
